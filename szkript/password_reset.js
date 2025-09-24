94dad53ea66d4500373c2c6e508af7556fcf3645
IvanyosLaszlo <ivanyoslaszlo25@gmail.com>
2025-09-24 15:18:53 +0200

    css javitás

--- szkript/password_reset.js
+++ szkript/password_reset.js
@@ -1,24 +1,43 @@
 const kuldes_gomb = document.getElementById("jelszo");
+const first_password = document.getElementById("first_password")
+const second_password = document.getElementById("second_password")
+
 
 kuldes_gomb.addEventListener("click", function () {
-    const input = document.getElementById("text");
-    const password = input.value;
 
-    fetch("http://localhost:8080/api/reset_password", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: "include",
-        body: JSON.stringify({ password })
-    })
-        .then(res => res.text())
-        .then(data => {
-            input.value = "";
-            alert(data);
+
+    if (first_password.value.trim() === "" || second_password.value.trim() === "") {
+        alert("Kérlek töltsd ki mindkét mezőt!");
+    }
+    else if
+        (first_password.value != second_password.value) {
+        alert("nem egyeznek a jelszavak!")
+    }
+
+    else {
+        const input = document.getElementById("first_password");
+        const password = input.value;
+
+        fetch(url + "/reset_password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({ password })
         })
-        .catch(err => {
-            alert("Hiba: " + err.message);
-        });
+            .then(res => res.text())
+            .then(data => {
+                input.value = "";
+                alert(data);
+                first_password.value = "";
+                second_password.value = "";
+            })
+            .catch(err => {
+                alert("Hiba: " + err.message);
+            });
+    }
+
+
 });
 
